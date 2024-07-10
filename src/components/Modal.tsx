91d0b5d5223744f427996f771937f469736771cf
paiva <lcspaiva87@gmail.com>
2024-07-09 22:25:08 -0300

    feat: Update Modal component to use dynamic title

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,19 +5,20 @@ import { ReactNode } from "react";
 interface ModalProps {
 
   setIsOpen: (value: boolean) => void;
-
+  title: string
   children: ReactNode;
 
 }
 export function Modal({
   setIsOpen,
-  children
+  children,
+  title
 }: ModalProps) {
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center ">
       <div className="bg-zinc-900 px-6 py-5 w-[360px]">
         <div className="flex justify-between">
-          <h1 className="text-lg font-semibold">Selecione a data de viagem</h1>
+          <h1 className="text-lg font-semibold">{title}</h1>
           <button className="size-3"
             onClick={() => setIsOpen(false)}
           >
