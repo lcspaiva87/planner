1094ef6737fb3a8fa8f8ccafefc73be64dc3babe
paiva <lcspaiva87@gmail.com>
2024-07-11 20:53:05 -0300

    chore: Update Modal component to include email input field for trip attendees

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,22 +3,28 @@ import { ReactNode } from "react";
 
 
 interface ModalProps {
-
   setIsOpen: (value: boolean) => void;
   title: string
   children: ReactNode;
+  subTitle?: string
+  width?: string
 
 }
 export function Modal({
   setIsOpen,
   children,
-  title
+  title,
+  subTitle,
+  width = '360px'
 }: ModalProps) {
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center ">
-      <div className="bg-zinc-900 px-6 py-5 w-[360px]">
-        <div className="flex justify-between">
-          <h1 className="text-lg font-semibold">{title}</h1>
+      <div className={`bg-zinc-900 px-6 py-5 w-[${width}]`}>
+        <div className="flex justify-between w-full">
+          <div className="flex flex-col text-start gap-2">
+            <h1 className="text-lg font-semibold">{title}</h1>
+            <h2 className="text-sm text-zinc-400">{subTitle}</h2>
+          </div>
           <button className="size-3"
             onClick={() => setIsOpen(false)}
           >
@@ -27,6 +33,6 @@ export function Modal({
         </div>
         {children}
       </div>
-    </div>
+    </div >
   )
 }
--- src/components/Selectguests.tsx
+++ src/components/Selectguests.tsx
@@ -1,5 +1,7 @@
-import { ArrowRight, UserPlus2 } from "lucide-react";
+import { ArrowRight, AtSign, Plus, UserPlus2, X } from "lucide-react";
+import { useState } from "react";
 import { DateRange } from "react-day-picker";
+import { Modal } from "./Modal";
 interface SelectguestsProps {
   email: string;
   setEmail: (email: string) => void;
@@ -15,17 +17,29 @@ export function Selectguests({
   setEmail,
   setIsGuestsInputOpen
 }: SelectguestsProps) {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <div className="bg-zinc-900 shadow-shape items-center gap-5 pl-4 pr-4 h-16 flex rounded-xl ">
       <div className="flex items-center gap-2 flex-1">
-        <UserPlus2 className="text-zinc-400 size-7" />
-        <input
+        <button
+          onClick={() => setIsOpen(true)}
+          className="flex items-center gap-2 text-left w-[240px]"
+        >
+          <UserPlus2 className="text-zinc-400 size-4" />
+          <span
+            className="text-sm text-zinc-400 w-40 flex-1 "
+          >
+            {'Quem estará na viagem?'}
+          </span>
+        </button>
+
+        {/* <input
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           type="text"
           placeholder="Quem estará na viagem?"
           className="placeholder:text-zinc-400 text-zinc-100 font-normal text-lg bg-transparent w-full focus:outline-none placeholder:text-sm"
-        />
+        /> */}
       </div>
 
       <button
@@ -35,6 +49,46 @@ export function Selectguests({
         Confirmar viagem
         <ArrowRight className="size-4" />
       </button>
+      {
+        isOpen && (
+          <Modal
+            width="640px"
+            title="Selecionar convidados"
+            subTitle="Os convidados irão receber e-mails para confirmar a participação na viagem."
+            setIsOpen={setIsOpen}           >
+            <div className="flex w-full flex-col">
+              <div className="flex flex-wrap w-[596px] gap-2">
+                {Array.from({ length: 10 }).map((_, index) => (
+                  <div key={index} className="px-2.5 py-1.5 flex justify-center gap-2.5 rounded-md bg-zinc-800 items-center">
+                    <span>emile.mayer25@yahoo.com</span>
+                    <button>
+                      <X className="size-4" />
+                    </button>
+                  </div>
+                ))}
+              </div>
+
+              <div className="w-full h-px bg-zinc-800" />
+              <div className=" rounded-lg border border-zinc-800 bg-zinc-950 px-4 py-2.5 flex justify-between mt-2">
+                <div className="flex items-center gap-2">
+                  <AtSign className="text-zinc-400 size-4" />
+                  <input type="email"
+                    placeholder="Digite o e-mail do convidado"
+                    className="placeholder:text-zinc-400 text-zinc-100 font-normal text-lg bg-transparent w-full focus:outline-none placeholder:text-sm"
+                  />
+                </div>
+                <button
+                  onClick={() => setIsGuestsInputOpen(true)}
+                  disabled={!location || !eventStartAndEndDates}
+                  className=" flex gap-2 px-5 py-2 bg-lime-300 items-center justify-center rounded-lg text-zinc-950">
+                  Convidar
+                  <Plus className="size-4" />
+                </button>
+              </div>
+            </div>
+          </Modal>
+        )
+      }
     </div>
   )
 }
