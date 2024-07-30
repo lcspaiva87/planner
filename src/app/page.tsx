7090fdde3418f3ba29605e25de9d7e512d7a2e46
paiva <lcspaiva87@gmail.com>
2024-07-29 22:19:39 -0300

    chore: Update page.tsx to include email input field for trip attendees

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
         )}
         <p className="text-sm text-zinc-500">
           Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
-          com nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade</a>.
+          com nossos <a className="text-zinc-300 underline" href="#">termos de uso.</a> e <a className="text-zinc-300 underline" href="#">políticas de privacidade.</a>.
         </p>
 
       </div>
